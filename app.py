06f030fb89b65aba249e9699071e4fb4127f524c
Pushparaj singh Jhala <158456288+PushprajSinghJhala@users.noreply.github.com>
2025-09-15 12:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,5 +162,7 @@ def create_app():
     return app
 
 if __name__ == "__main__":
+    app = create_app()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0",port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
